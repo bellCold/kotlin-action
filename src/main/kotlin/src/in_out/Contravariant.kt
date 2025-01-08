5140ccdcbd8b3781cd1f638d8135f10c32818776
bellCold <whdcks420@gmail.com>
2025-01-08 15:22:39 +0900

    in and out keyword

---
+++ src/main/kotlin/src/in_out/Contravariant.kt
@@ -0,0 +1,30 @@
+package src.in_out
+
+interface InExample<in T> {
+    fun consume(input: T)
+}
+
+interface OutExample<out T> {
+    fun returnValue(name: String):
+}
+
+class StringInExample : InExample<String> {
+    override fun consume(input: String) {
+        println("consume $input")
+    }
+}
+
+class StringOutExample : OutExample<String> {
+    override fun returnValue(name: String): String {
+        return "**** ${name} ****"
+    }
+}
+
+fun main() {
+    val example1 = StringInExample()
+    example1.consume("Just a string")
+
+    val example2 = StringOutExample()
+    val returnValue = example2.returnValue("start")
+    println(returnValue)
+}
